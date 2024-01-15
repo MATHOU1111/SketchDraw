d4b067c8d2585f93f376b4e23b96eded00bc9b67
mathou <mathisdumage0@gmail.com>
2024-01-15 22:12:39 +0100

    Added clear button + new bug

--- src/components/CanvasComponent.jsx
+++ src/components/CanvasComponent.jsx
@@ -1,32 +1,52 @@
-import { useEffect } from 'react';
+import { useEffect, useRef  } from 'react';
 import { fabric } from 'fabric';
-
+import { Button} from "@chakra-ui/react";
 
 
 function CanvasComponent() {
 
+    const canvasRef = useRef(null);
+
+    const canvasClear = () => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            console.log(canvas.clear())
+            canvas.isDrawingMode = false;
+        }
+    }
 
 
-    useEffect(() => {
-        const canvas = new fabric.Canvas('my-fabric-canvas', {
-            backgroundColor: 'white',
-            isDrawingMode: true
-        });
-        canvas.renderAll();
-    }, []);
+
+        useEffect(() => {
+            const canvas = new fabric.Canvas('my-fabric-canvas', {
+                backgroundColor: 'white',
+                isDrawingMode: true
+            });
+
+            canvasRef.current = canvas;
+            canvas.renderAll();
+        }, []);
 
 
+        return (
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+            }}>
 
-    return (
-        <div>
-            <canvas
-                id="my-fabric-canvas"
-                width="800"
-                height="600"
+                <canvas
+                    id="my-fabric-canvas"
+                    width="900"
+                    height="600">
+                </canvas>
+                <div>
+                    <Button onClick={canvasClear}> Clear </Button>
+                </div>
+            </div>
+        )
 
-            ></canvas>
-        </div>
-    );
 }
 
 export default CanvasComponent;
